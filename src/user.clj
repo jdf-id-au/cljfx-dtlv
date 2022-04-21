3eb0efb42e6e068d45c1a63f7fc4eab308aa7a75
Jeremy Field <jdf@jdf.id.au>
2022-04-21 12:31:35 +1000

    include context atom as well for ephemeral state

--- src/user.clj
+++ src/user.clj
@@ -1,31 +1,71 @@
 (ns user
   (:require [cljfx.api :as fx]
-            [datalevin.core :as dl]))
+            [datalevin.core :as dtlv]))
 
-(def con (dl/get-conn "store.dtlv"))
+;; Disk store but directly connected to UI
+(defonce dlc (dtlv/get-conn "store.dtlv"))
+#_(dtlv/close dlc)
 
-#_(dl/transact! con [{:a "hello there"}])
-#_(dl/transact! con [{:b "hm" :c "blah"} {:d 123}])
-#_(dl/q '[:find [(pull ?e [*]) ...] :where [?e _ _]] @con)
+;; Ephemera
+(defonce ctx (atom (fx/create-context {:ctx-ephemera []})))
+(defn touch-ctx
+  "Pretend context has changed. Causes (seemingly selective, non-flashy) rerender."
+  [& _] (swap! ctx fx/swap-context identity))
 
-(defn root [{:keys [::store]}]
+(defn root [{:keys [dtlv :fx/context]}]
   {:fx/type :stage
    :showing true
-   :title "hello"
+   :title "Persistent vs ephemeral state"
    :scene {:fx/type :scene
-           :root {:fx/type :v-box
+           :root {:fx/type :h-box
                   :children
-                  (into [{:fx/type :label :text "hmm"}])
-                  (for [v (dl/q '[:find [(pull ?e [*]) ...] :where [?e _ _]] store)]
-                    {:fx/type :label :text (str v)})}}})
+                  [{:fx/type :v-box
+                    :children
+                    (into [{:fx/type :button :text "datalevin" :on-action {:type :dl-event}}]
+                      (for [v (dtlv/q '[:find [(pull ?e [*]) ...] :where [?e _ _]] @dtlv)]
+                        {:fx/type :label :text (str v)}))}
+                   {:fx/type :v-box
+                    :children
+                    (into [{:fx/type :button :text "context atom" :on-action {:type :ctx-event}}]
+                      (for [v (fx/sub-val context :ctx-ephemera)]
+                        {:fx/type :label :text (str v)}))}]}}})
 
-(def renderer
-  (fx/create-renderer))
+(defn handler [{:keys [type]}]
+  (case type
+    :dl-event {:dl (rand)}
+    :ctx-event {:ctx (rand)}))
 
-(defn refresh-ui [{:keys [db-after]}]
-  (renderer {:fx/type root ::store db-after}))
+(defn dl-effect
+  "Return effect fn which updates disk store."
+  [dlc]
+  (fn [v _]
+    (dtlv/transact! dlc [{:dl-ephemeron v}])))
 
-(dl/listen! con :ui refresh-ui)
-#_(dl/unlisten! con :ui)
+(defn ctx-effect
+  "Return effect fn which updates app context."
+  [context]
+  (fn [v _]
+    (swap! context fx/swap-context update :ctx-ephemera conj v)))
 
-#_(renderer)
+(defn create-app
+  "Factored out for controllable rebinding during dev."
+  []
+  (fx/create-app ctx
+    :event-handler handler
+    :desc-fn #(assoc % :fx/type root :dtlv dlc)
+    :co-effects {:dtlv #(deref dlc) :ctx #(deref ctx)}
+    :effects {:dl (dl-effect dlc)
+              :ctx (ctx-effect ctx)}))
+
+(defonce app (create-app))
+
+(defn- rebind-app!
+  "(dev conveience)"
+  []
+  (fx/unmount-renderer ctx (:renderer app))
+  (def app (create-app)))
+
+(dtlv/listen! dlc :ui touch-ctx)
+#_(dtlv/unlisten! dlc :ui)
+
+#_(rebind-app!)
