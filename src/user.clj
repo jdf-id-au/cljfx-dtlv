ec7cfb25e01392ec5ddd1a4430e6bdcee348ec1a
Jeremy Field <jdf@jdf.id.au>
2022-04-25 11:09:18 +1000

    found working, context-compatible pattern

--- src/user.clj
+++ src/user.clj
@@ -2,18 +2,23 @@
   (:require [cljfx.api :as fx]
             [datalevin.core :as dtlv]))
 
-;; Disk store but directly connected to UI
 (defonce dlc (dtlv/get-conn "store.dtlv"))
 #_(dtlv/close dlc)
 
-;; Ephemera
-(defonce ctx (atom (fx/create-context {:ctx-ephemera []})))
-(defn touch-ctx
-  "Pretend context has changed. Causes (seemingly selective, non-flashy) rerender."
-  [& _] (swap! ctx fx/swap-context identity))
+(defonce ctx (atom (fx/create-context {:ctx-ephemera [] :dtlv dlc})))
 
-(defn root [{:keys [dtlv :fx/context]}]
+(dtlv/listen! dlc :ui (fn touch-ctx [_] (swap! ctx fx/swap-context assoc :max-tx (:max-tx @dlc))))
+#_(dtlv/unlisten! dlc :ui)
+
+(defn dlq-sub [ctx query-vec & args]
+  (let [_ (fx/sub-val ctx :max-tx) ; <- this dependency causes automatic refresh via listen! above
+        db @(fx/sub-val ctx :dtlv)]
+    (apply dtlv/q query-vec (cons db args))))
+
+(defn root [{:keys [:fx/context]}]
   {:fx/type :stage
+   :width 500
+   :height 500
    :showing true
    :title "Persistent vs ephemeral state"
    :scene {:fx/type :scene
@@ -22,7 +27,8 @@
                   [{:fx/type :v-box
                     :children
                     (into [{:fx/type :button :text "datalevin" :on-action {:type :dl-event}}]
-                      (for [v (dtlv/q '[:find [(pull ?e [*]) ...] :where [?e _ _]] @dtlv)]
+                      (for [v (fx/sub-ctx context dlq-sub
+                                '[:find [(pull ?e [*]) ...] :where [?e _ _]])]
                         {:fx/type :label :text (str v)}))}
                    {:fx/type :v-box
                     :children
@@ -43,9 +49,9 @@
 
 (defn ctx-effect
   "Return effect fn which updates app context."
-  [context]
+  [ctx]
   (fn [v _]
-    (swap! context fx/swap-context update :ctx-ephemera conj v)))
+    (swap! ctx fx/swap-context update :ctx-ephemera conj v)))
 
 (defn create-app
   "Factored out for controllable rebinding during dev."
@@ -53,19 +59,15 @@
   (fx/create-app ctx
     :event-handler handler
     :desc-fn #(assoc % :fx/type root :dtlv dlc)
-    :co-effects {:dtlv #(deref dlc) :ctx #(deref ctx)}
     :effects {:dl (dl-effect dlc)
               :ctx (ctx-effect ctx)}))
 
 (defonce app (create-app))
 
 (defn- rebind-app!
-  "(dev conveience)"
+  "(dev convenience)"
   []
   (fx/unmount-renderer ctx (:renderer app))
   (def app (create-app)))
-
-(dtlv/listen! dlc :ui touch-ctx)
-#_(dtlv/unlisten! dlc :ui)
 
 #_(rebind-app!)
